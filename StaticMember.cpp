26946de1816d80a74bfe0d32f2dcab3dc62ce3e4
Aseel <asilalhashmi@email.com>
2026-05-12 10:11:42 +0700

    .

---
+++ StaticMember.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+class Mahasiswa 
+{
+public :
+	static int nim;
+	int id;
+	string name;
+
+	void setID();
+	void printAll();
+
+	Mahasiswa(string pnama) :name(pnama)
+	{
+		setID();
+	}
+};
+
+int Mahasiswa::nim = 10;
+
+void Mahasiswa::setID() 
+{
+	id = ++nim;
+}
+void Mahasiswa::printAll() 
+{
+	cout << "ID : " << id << endl;
+	cout << "Name : " << name << endl;
+	cout << endl;
+
+}
+
+
+int main() 
+{
+	Mahasiswa mhs1("Aseel");
+	Mahasiswa mhs2("Ahmed");
+	Mahasiswa mhs3("Ali");
+	Mahasiswa mhs4("Hussain");
+
+	mhs1.printAll();
+	mhs2.printAll();
+	mhs3.printAll();
+	mhs4.printAll();
+
+}
