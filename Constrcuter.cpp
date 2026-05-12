08aa0c6d4b3b1a5aadc914689db14b379d859357
Aseel <asilalhashmi@email.com>
2026-05-12 10:35:24 +0700

    .

--- Constrcuter.cpp
+++ Constrcuter.cpp
@@ -1,6 +1,18 @@
 #include<iostream>
 using namespace std;
+
+class Mahasiswa 
+{
+public:
+	Mahasiswa();
+};
+
+Mahasiswa::Mahasiswa() 
+{
+	cout << "Constructer " << endl;
+}
 int main() 
 {
-
+	Mahasiswa mhs;
+	return 0;
 }
