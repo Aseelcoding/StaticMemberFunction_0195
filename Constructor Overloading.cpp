19d9abf134cf51d40b6ad2cd51ab7152affa657f
Aseel <asilalhashmi@email.com>
2026-05-12 16:12:40 +0700

    Added multiple constructors Used constructor overloading Allowed different object initialization methods

--- Constructor Overloading.cpp
+++ Constructor Overloading.cpp
@@ -8,8 +8,24 @@ private:
     string nama;
 
 public:
+    mahasiswa();
+    mahasiswa(int);
+    mahasiswa(string);
+    mahasiswa(int iNim, string iNama);
     void cetak();
 };
+mahasiswa::mahasiswa(int iNim) {
+    nim = iNim;
+}
+
+mahasiswa::mahasiswa(string iNama) {
+    nama = iNama;
+}
+
+mahasiswa::mahasiswa(int iNim, string iNama) {
+    nim = iNim;
+    nama = iNama;
+}
 int main() 
 {
 
