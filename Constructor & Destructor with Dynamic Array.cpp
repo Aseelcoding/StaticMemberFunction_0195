75eeb85cdf09725b7de9fd4705b98fd543b869f6
Aseel <asilalhashmi@email.com>
2026-05-12 16:15:03 +0700

    Added function to input data Added function to display array values Used loops for array processing

--- Constructor & Destructor with Dynamic Array.cpp
+++ Constructor & Destructor with Dynamic Array.cpp
@@ -8,10 +8,27 @@ private:
     int panjang;
 
 public:
+    void cetakData();
+    void isiData();
     angka(int);
     ~angka();
 };
+void angka::cetakData()
+{
+    for (int i = 1; i <= panjang; i++)
+    {
+        cout << i << " = " << arr[i] << endl;
+    }
+}
 
+void angka::isiData()
+{
+    for (int i = 1; i <= panjang; i++)
+    {
+        cout << i << " = ";
+        cin >> arr[i];
+    }
+}
 int main() 
 {
 
