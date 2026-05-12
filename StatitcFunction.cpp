8bfbc24c70cd4a8b800f8a50df59ff42acc1b4f1
Aseel <asilalhashmi@email.com>
2026-05-12 10:13:06 +0700

    .

--- StatitcFunction.cpp
+++ StatitcFunction.cpp
@@ -1,5 +1,18 @@
 #include <iostream>
 using namespace std;
+class Mahasiswa 
+{
+private :
+	static int nim;
+
+public :
+	int id;
+	string name;
+
+	void setID();
+	void PrintAll();
+};
+
 
 int main() 
 {
